46056f05ea2a8c04d3d7b8e5ee50d026f68e64aa
brandonEsquivel <56413453+brandonEsquivel@users.noreply.github.com>
2020-10-30 20:25:46 -0600

    update LRU Rep. Policy
    the LRU Replacement policy is added, needs the check_params function and the corrrect includes path

--- src/L1cache.cpp
+++ src/L1cache.cpp
@@ -99,8 +99,147 @@ int lru_replacement_policy (int idx,
                              operation_result* result,
                              bool debug)
 {
-   return ERROR;
+   
+  /*LRU Policy:
+The LRU algorithm (less recently used for its acronym in English), tries to reduce the possibility
+of discarding blocks that are going to be used again in the near future. For this, the information
+regarding the use of the blocks is stored so that in a replacement the block that has not been used
+for a longer time is discarded. LRU is based on the principle of temporary locality, according to which
+if a block has been used recently it is likely to be used again in the near future, hence the best
+candidate to replace is the block that has not been used for more. weather. The implementation of this
+algorithm requires keeping a record of the use of each block so that updating the use of one block
+implies updating that of all the others, hence this implementation tends to be very expensive for
+highly associativity caches. This algorithm works as a queue-like data structure, hence it is referred
+to as the LRU queue. The beginning of the queue (head), and therefore the candidate to leave it first,
+is the block that has been referenced least recently (LRU block). Likewise, the end of the queue (queue)
+and therefore the candidate to stay longer in it, is the block that has been most recently referenced
+(MRU block, from the most recently used English). Since this algorithm is based on the principle of
+temporal locality, it performs well against loads with high temporal locality in the data, but in
+the case of loads where new references occur in the distant future, LRU performs poorly. As an example of the latter
+type of loads there is the case in which the working set is greater than the capacity of the cache or
+when a group of data without any type of reuse supposes the replacement of the data of the working set.*/
+int lru_replacement_policy (int idx,
+                             int tag,
+                             int associativity,
+                             bool loadstore,
+                             entry* cache_blocks,
+                             operation_result* result,
+                             bool debug)
+{
+  // Control Flags
+  bool cache_full = true;          // for eviction
+  bool Hit_flag = false;           // one flag 1 for hit, 0 for miss
+  
+  // Ints
+  int MRU_value = associativity - 1;      // Most recently used value
+  int LRU_value = 0;                      // Lest recently used value.
+  int Victim_value = -1;                        // Victim  value
+  int hit_way = -1;                       // number of the hit way analyzed
+  int free_way = -1;                       // number of the free way analyzed
+  
+  // getting parameters checked
+  int param_check = params_check(idx, tag, associativity);      // returns Ok or PARAM
+  if (param_check!=OK){
+    return PARAM;
+  }
+  // checking all the ways
+  for (int way = 0; way < associativity; way++){
+
+    // LETS CHECK IF Hit
+    if (cache_blocks[way].valid && cache_blocks[way].tag == tag){     // There will only be hit if the block in the current way is valid and the tag is the one indicated
+      Hit_flag = true;
+      hit_way = way;
+    }
+    //Space check, tracking of free ways and full cache
+    if (cache_blocks[way].valid == false) {
+      cache_full = false;
+      free_way = way;
+    }
+  }
+  // check hit status
+  if (Hit_flag){
+    //if was a Store
+    if (loadstore){
+      result->miss_hit = HIT_STORE;  
+    //if was a Load
+    }else{
+      result->miss_hit = HIT_LOAD;
+    }
+    result->evicted_address = 0;      // not an address
+    result->dirty_eviction = false; // not evicted
+
+    // checking the replacement policy value
+    for (int i = 0; i < associativity; i++){                // across all ways
+      if (cache_blocks[i].rp_value>cache_blocks[hit_way].rp_value){   // limits of desicion
+        if (cache_blocks[i].rp_value!=LRU_value){             
+          cache_blocks[i].rp_value = cache_blocks[i].rp_value - 1;  // LRU value substraccion, policy.
+        }
+      }
+    }
+    cache_blocks[hit_way].rp_value = MRU_value; // now if the hit gets the maximun replacement value
+    if(loadstore){     // Under a operation whit hit
+      cache_blocks[hit_way].dirty = true;
+    }
+  }else{ // if we have a Miss
+    if (loadstore){    // operation Store
+      result->miss_hit = MISS_STORE;   // a store with a miss
+
+    }else{     // If we have a Load
+      result->miss_hit = MISS_LOAD;     // a Load with a miss
+    }
+    if (cache_full == false){     // now case of Free Space, if is not full
+      result->dirty_eviction = false;     // not necessary an eviction
+      result->evicted_address = 0;        // setting address to zero
+      // checking all ways.
+      for (int i = 0; i < associativity; i++){
+        if (cache_blocks[i].valid){       // the block is valid
+          if (cache_blocks[i].rp_value != LRU_value){    // the same logic, to update de Replacement policy value
+            cache_blocks[i].rp_value = cache_blocks[i].rp_value - 1;  // substraction on the RP value
+            }
+        }
+      }
+      if (loadstore){    // lets check the dirty bit
+        cache_blocks[free_way].dirty = true;   // if it was a operation over the block, set the dirty bit
+      }else{
+        cache_blocks[free_way].dirty = false;     // clean by default
+      }
+      cache_blocks[free_way].rp_value = MRU_value;  // now if the hit gets the maximun replacement value, the most recently used value.
+      cache_blocks[free_way].tag = tag;             // update tag
+      cache_blocks[free_way].valid = true;          // update valid
+    }else{          // Now we have the last case: if cache is full
+      // again checking all ways
+      for (int i = 0; i < associativity; i++){
+        if (cache_blocks[i].rp_value == LRU_value){   // checking for the victim 
+          Victim_value = i;     //now set the Victim value
+        }else{
+          cache_blocks[i].rp_value = cache_blocks[i].rp_value -1;  // substraction on the RP value
+        }
+      }
+      result->evicted_address = cache_blocks[Victim_value].tag;       // saving to result the address of the victim block with tag
+      if (cache_blocks[Victim_value].dirty){
+        result->dirty_eviction = true;        // update dirty eviction 
+      }else{
+        result->dirty_eviction = false;
+      }
+  
+      if (loadstore){     //Now is necessary to check the dirty bits of the victims in order to know if store/load is needed
+        cache_blocks[Victim_value].dirty = true;
+      }else{
+        cache_blocks[Victim_value].dirty = false;
+      }
+      cache_blocks[Victim_value].rp_value = MRU_value;    // again back and gets the maximun replacement value, the most recently used value.
+      cache_blocks[Victim_value].valid = true;
+      cache_blocks[Victim_value].tag = tag;
+
+
+    }
+  }
+  return OK;        // param type return 
 }
+  
+  
+  // ------------- END - ESQUIVEL, BRANDON ----------------
+  
 int nru_replacement_policy(int idx,
                            int tag,
                            int associativity,
@@ -108,71 +247,4 @@ int nru_replacement_policy(int idx,
                            entry* cache_blocks,
                            operation_result* operation_result,
                            bool debug)
-{
-for(int i=0; i < associativity; i++)
-	{
-		if(cache_blocks[idx*associativity+i].tag == tag) /*HIT*/
-		{
-			if(loadstore == 0) /*Load*/
-			{
-				H_L = H_L+1;
-			}
-			else /*Store*/
-			{
-				H_S = H_S+1;
-			}
-			cache_blocks[idx*associativity+i].rp_value = 0;
-			cache_blocks[idx*associativity+i].dirty = 0; /* No write */
-		}
-		else
-		{
-			if(i == associativity-1) /*Set is done?*/ /*MISS*/
-			{
-				if(loadstore == 0) /*Load*/
-				{
-					M_L = M_L+1;
-				}
-				else /*Store*/
-				{
-					M_S = M_S+1;
-				}
-				
-				/*Victim search*/
-				bool rep_ok = false;
-				while(rep_ok==false)
-				{
-					for(int j=0; j < associativity; j++)
-					{
-						if(cache_blocks[idx*associativity+j].rp_value == 1) /*Victim found*/
-						{
-							if(cache_blocks[idx*associativity+j].dirty = 1)
-							{
-								operation_result[idx*associativity+j].dirty_eviction = 1;
-								operation_result[idx*associativity+j].evicted_address = 1;
-							}
-							
-							/*Replacement*/	
-							cache_blocks[idx*associativity+j].rp_value = 0;
-							cache_blocks[idx*associativity+j].tag = tag;
-							cache_blocks[idx*associativity+j].valid = 1;
-							cache_blocks[idx*associativity+j].dirty = 1;
-							
-							rep_ok = true;	
-						}
-						else
-						{
-							if(j == associativity-1) /*Set is done?*/ /*There's no 1 found*/
-							{
-								for(int k=0; k < associativity; k++)
-								{
-									cache_blocks[idx*associativity+k].rp_value = 1;
-								}
-							}
-						}
-					}
-				}	
-			}
-		}
-	}
-   return ERROR;
-}
+{ return ERROR;
