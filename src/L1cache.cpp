883d63ccdd443e745731b29c9109f1ab4c0d158f
Belinda Brown <belindabrownr04@gmail.com>
2020-10-31 23:57:21 -0600

    comments srrip ✅

--- src/L1cache.cpp
+++ src/L1cache.cpp
@@ -223,6 +223,30 @@ int srrip_replacement_policy (int idx,
    //*********************
    else if ((empty_found_YorN == false) && (hit_found_YorN == false))
    {
+////////////////////////////////////////////////////////////////////	   	   
+// https://www.tutorialspoint.com/cplusplus/cpp_goto_statement.htm
+// #include <iostream>
+// using namespace std;
+ 
+// int main () {
+//    // Local variable declaration:
+//    int a = 10;
+
+//    // do loop execution
+//    LOOP:do {
+//       if( a == 15) {
+//          // skip the iteration.
+//          a = a + 1;
+//          goto LOOP;
+//       }
+//       cout << "value of a: " << a << endl;
+//       a = a + 1;
+//    } 
+//    while( a < 20 );
+ 
+//    return 0;
+// }	
+////////////////////////////////////////////////////////////////////	   
    ImmediateFuture:
       if (higher_RRPV == true)
       {
