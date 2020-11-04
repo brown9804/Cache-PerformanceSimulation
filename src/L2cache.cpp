b412bca83f960b6e5d7bf1c4da5e36d19679e4fd
Belinda Brown <belindabrownr04@gmail.com>
2020-11-04 05:07:54 -0600

    going to test 0.0.0

--- src/L2cache.cpp
+++ src/L2cache.cpp
@@ -39,6 +39,20 @@ using namespace std;
 // *.Between L2 and memory it is used:
 // - writeback policy.
 
+// ****
+///A second level of off-chip (L2) cache
+// is added with less access time than main memory.
+// ****
+// STEPS:
+// 1. HITS_L1 - Analyze if hits in L1:
+// When a primary cache (L1) failure occurs,
+// 2. Depends on load - store do same thing on l2:
+// the secondary (L2) cache is accessed to search for data.
+// 3. Analize the eviction - L1 & L2 .. Here?
+//////////// * /////////////
+// - If they are there, the failure penalty is reduced.
+// - If they are not, we access the main memory.
+
 int lru_replacement_policy_l1_l2(const entry_info *l1_l2_info,
 								 bool loadstore,
 								 entry *l1_cache_blocks,
@@ -47,5 +61,90 @@ int lru_replacement_policy_l1_l2(const entry_info *l1_l2_info,
 								 operation_result *l2_result,
 								 bool debug)
 {
-	return OK;
+///////////////////////////////////////
+// Variables
+///////////////////////////////////////
+//// L1
+int l1l2_l1_info_tag = l1_l2_info->l1_tag;
+int l1l2_l1_info_idx = l1_l2_info->l1_idx;
+int l1l2_l1_info_assoc = l1_l2_info->l1_assoc;
+/// L2
+int l1l2_l2_info_tag = l1_l2_info->l2_tag;
+int l1l2_l2_info_idx = l1_l2_info->l2_idx;
+int l1l2_l2_info_assoc = l1_l2_info->l2_assoc;
+// GENERAL
+int i = 0;
+///FLAGS 
+bool L1_HITS = false;
+
+///////////////////////////////////////
+// LOGIC 
+///////////////////////////////////////
+/////// ANALIZE --- FAILURE OCCURS...
+//// DIRTY  --- EVICTION
+while (i < l1l2_l1_info_assoc) {
+	i = i + 1;
+	l1_result->dirty_eviction = 0;
+	l1_cache_blocks[i].dirty = 0;
+	////  1. HITS_L1 - Analyze if hits in L1
+	if (L1_HITS == false && (l1_cache_blocks[i].tag == l1l2_l1_info_tag))
+	{
+		L1_HITS = true;
+	}
+}
+
+if (L1_HITS == false) // 3. Analize the eviction - L1 & L2 .. Here?
+{
+	//////// L1
+	lru_replacement_policy(l1l2_l1_info_idx,
+						   l1l2_l1_info_tag,
+						   l1l2_l1_info_assoc,
+						   loadstore,
+						   l1_cache_blocks,
+						   l1_result,
+						   debug);
+	//////// L2
+	lru_replacement_policy(l1l2_l2_info_idx,
+						   l1l2_l2_info_tag,
+						   l1l2_l2_info_assoc,
+						   loadstore,
+						   l2_cache_blocks,
+						   l2_result,
+						   debug);
+}
+
+//// BECAUSE L1 FAILURE OCCUR ---- 
+/////// L2 WORKING...
+/// Is accessed to search for data
+else // 2. Depends on load - store do same thing on l2:
+	{
+		lru_replacement_policy(l1l2_l2_info_idx,
+							   l1l2_l2_info_tag,
+							   l1l2_l2_info_assoc,
+							   loadstore,
+							   l2_cache_blocks,
+							   l2_result,
+							   debug);
+
+		//////////////////
+		//////// Store // 1 true
+		///////////////////
+		if (loadstore == 1)
+		{
+			l2_result->miss_hit = HIT_STORE;
+		}
+
+		//////////////////
+		//////// Load // 0 false
+		///////////////////
+		else
+		{
+			l2_result->miss_hit = HIT_LOAD;
+		}
+
+}
+
+
+
+return OK;
 }
